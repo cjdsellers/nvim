1842892a2ccdba1a6109c2cc11be05c63f85fd10
Chris Sellers <chris@cjdsellers.io>
2023-03-30 08:48:55 +1100

    Add guidelines

--- options.lua
+++ options.lua
@@ -8,6 +8,7 @@ return {
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
     cmdheight = 1, -- sets vim.opt.cmdheight
+    colorcolumn = "100,120",
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
