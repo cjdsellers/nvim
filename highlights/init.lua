640d7a56eabcfd8b66a1baa46111b4bc0f2d5e7a
Chris Sellers <chris@cjdsellers.io>
2023-07-20 12:16:07 +1000

    Tweak NormalFloat colour

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,5 +1,5 @@
 return { -- this table overrides highlights in all themes
   Normal = { bg = "#202020" },
-  NormalFloat = { bg = "#252525" },
+  NormalFloat = { bg = "#282828" },
   SignColumn = { bg = "#202020" },
 }
