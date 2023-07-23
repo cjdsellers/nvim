4207c719b38df22dce285c19a8127b56878aabad
Chris Sellers <chris@cjdsellers.io>
2023-07-23 21:24:34 +1000

    Align NormalFloat with Normal

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,5 +1,5 @@
 return { -- this table overrides highlights in all themes
   Normal = { bg = "#202020" },
-  NormalFloat = { bg = "#282828" },
+  NormalFloat = { bg = "#202020" },
   SignColumn = { bg = "#202020" },
 }
