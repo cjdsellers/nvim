8af5c058aafd4abd31f9785ae548ad51269651c9
Chris Sellers <chris@cjdsellers.io>
2023-08-14 19:35:03 +1000

    Make backgrounds transparent

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,5 +1,8 @@
 return { -- this table overrides highlights in all themes
   Normal = { guibg = NONE },
+  NormalFloat = { guibg = NONE },
+  SignColumn = { guibg = NONE },
+
   -- Normal = { bg = "#202020" },
   -- NormalFloat = { bg = "#202020" },
   -- SignColumn = { bg = "#202020" },
