4539a23ba7a52e5b6666a6d60d4a5c28ac6f39c7
Chris Sellers <chris@cjdsellers.io>
2023-08-15 11:11:39 +1000

    Fix notify background

--- highlights/init.lua
+++ highlights/init.lua
@@ -2,6 +2,7 @@ return { -- this table overrides highlights in all themes
   Normal = { bg = "NONE" },
   NormalFloat = { bg = "NONE" },
   SignColumn = { bg = "NONE" },
+  NotifyBackground = { bg = "#202020" },
 
   -- Normal = { bg = "#202020" },
   -- NormalFloat = { bg = "#202020" },
