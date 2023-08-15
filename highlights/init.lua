034ed669a8e506f04325a524922455986cfc54e2
Chris Sellers <chris@cjdsellers.io>
2023-08-15 11:01:24 +1000

    Fix transparent background

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,7 +1,7 @@
 return { -- this table overrides highlights in all themes
-  Normal = { guibg = NONE },
-  NormalFloat = { guibg = NONE },
-  SignColumn = { guibg = NONE },
+  Normal = { bg = "NONE" },
+  NormalFloat = { bg = "NONE" },
+  SignColumn = { bg = "NONE" },
 
   -- Normal = { bg = "#202020" },
   -- NormalFloat = { bg = "#202020" },
